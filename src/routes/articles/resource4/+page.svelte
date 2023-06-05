32e167dafd90b474c7a95a40276f9068e5e33532
Mason Wheeler <mwheel@uw.edu>
2023-06-05 16:53:41 -0700

    added resource 4

---
+++ src/routes/articles/resource4/+page.svelte
@@ -0,0 +1,64 @@
+<h1>
+      Specialists in politics and administration — both within and outside of
+government—are forging closer working relationships as part of
+policy-making processes through New Media networks variously known as
+web blogs , portals and wikis. These and other internet links allow
+contact between computer or smart phone users, exchange of information
+and the formation of mutually supportive issue networks as alternatives to
+information provided by traditional business, government and media
+sources (Barlow, 2007a; 2007b; Feld and Wilcox, 2008; Gill, 2004;
+Kahler, 2009). Portals such as the USA.gov website are organized from a
+citizen-centric perspective. The website is a virtual one-stop-shop to
+government services and information.2 In the pre-internet era, citizens
+would have to contact individual government offices to apply for and
+receive information — a time-consuming task which would discourage all
+but the most tenacious. The one-stop-shops, whether they are virtual or
+physical, represent an organizational change from department-task
+orientation to citizen-oriented service. Blogging and social networking
+have accelerated this trend and become pervasive sources of citizen
+participation covering a wide range of political and social issues.
+Although its full influence is still unknown, millions of participants in
+the developing public space or “blogosphere” are influencing political
+processes by acting as grassroots observers, reporters, reviewers and
+fact-checkers (Gill, 2004: 5; Habermas, 1991; Keren, 2006; Richardson,
+2006). Citizens are using blogs, e-mail, smart phones and social
+networking to participate in web-based political and social activities and
+express their views and positions on a wide range of policy issues. This new
+type of amateur journalism and political and social commentary challenges
+established news outlets and opinion sources by promoting a more open,
+participatory culture in nearly every aspect of society. As a result, demand
+for expertise in decision making and policy analysis has increased,
+reflecting human resource systems based on job-related competence,
+expanded ICT networks and the need for advanced knowledge of New
+Media technologies. At the same time, interest groups, politicians and
+public officials are also applying these same technologies to conduct
+political campaigns, communicate with constituents and recruit new
+members and voters (Hill, 2009; Miller, 2008). Mastery of the new
+technology—in all its forms—has become a virtual prerequisite for
+successful lobbying, winning political campaigns and providing better
+service to constituents (Chapter 2).
+The 2008 U.S. presidential election represented a major breakthrough
+in the application of New Media technologies to politics and political
+campaigns. The extensive use of ICTs for political communication began
+Milakovich, M. E. (2011). Digital governance : New technologies for improving public service and participation. Taylor & Francis Group.
+Created from washington on 2022-05-01 18:56:59.
+Copyright © 2011. Taylor & Francis Group. All rights reserved.
+even earlier with former presidential candidate Howard Dean’s failed run
+for the presidency in 2004 (Hindman, 2006). After being appointed
+Democratic National Committee (DNC) Chair in 2005, Dean (together
+with President Obama’s tech-savvy campaign advisers) used ICTs so
+effectively during the Democrats’ grassroots 2008 campaign that they
+may have significantly impacted the results of the presidential election.
+Chapter 2 shows how this type of e-campaigning uses ICTs as a means to
+disseminate information, mobilize volunteers, raise funds and otherwise
+pursue electioneering functions (Garson, 2006: 69–72). Plainly, ICTs
+played a significant role in promoting additional citizen participation
+during the recent U.S. elections. The causal linkages between decision
+making, fund raising, party identification, political activism, social
+networking and voter turnouts are more difficult to establish with
+precision: they are currently being explored in more depth by communica-
+tions experts and political scientists. Recent studies suggest that more
+Americans are using ICTs to access information, ask questions and receive
+government services online (Pew Internet and American Life Project,
+2010 ).
+</h1>
